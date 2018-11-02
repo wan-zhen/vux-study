9d6ea3d92ffde55331d4c8c58e3ec53d390fc57b
wan-zhen <kelly811124811124@hotmail.com>
2018-11-02 14:11:59 +0800

    install vux & 加入 button component到 main 當全域組件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
+import router from "./router";
+
+// 全域組件還沒辦法用 form vux 直接 import 打包过大问题临时解决方案 直接指定要引入的组件全路径
+// 直接在組件 import 是可以直接用 vux 的
+import XButton from "vux/src/components/x-button/index.vue";
+
+Vue.component("XButton", XButton);
 
 Vue.config.productionTip = false;
-
 new Vue({
+  router,
   render: h => h(App)
 }).$mount("#app");
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <x-button type="primary">primary</x-button>
+    <x-button type="warn">Delete</x-button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
