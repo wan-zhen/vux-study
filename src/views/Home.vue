5cf5086be96c9ff85a5b7dca5bd42a1567ad19b0
wan-zhen <kelly811124811124@hotmail.com>
2018-11-02 17:07:28 +0800

    設定 button-tab

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <x-button type="primary">primary</x-button>
     <x-button type="warn">Delete</x-button>
-     <font-awesome-icon icon="coffee" />
+    <font-awesome-icon icon="coffee" />
       <tabbar>
         <tabbar-item selected show-dot>
+          <img slot="icon" src="../assets/logo.png">
           <span slot="label">遊戲</span>
         </tabbar-item>
         <tabbar-item show-dot>
+          <img slot="icon" src="../assets/logo.png">
           <span slot="label">帳務中心</span>
         </tabbar-item>
         <tabbar-item>
+          <img slot="icon" src="../assets/logo.png">
           <span slot="label">在線客服</span>
         </tabbar-item>
         <tabbar-item badge="12">
+          <img slot="icon" src="../assets/logo.png">
           <span slot="label">優惠活動</span>
         </tabbar-item>
         <tabbar-item badge="2">
+          <img slot="icon" src="../assets/logo.png">
           <span slot="label">我的</span>
         </tabbar-item>
       </tabbar>
